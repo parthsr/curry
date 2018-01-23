43b3099c7e974be96ae61dfbf19b96f55a676e6f
parth <srivastavaparth2016@gmail.com>
2018-01-23 23:19:02 +0530

    adding a feew test cases for callapply.js

--- ex5/callapply.js
+++ ex5/callapply.js
@@ -1,9 +1,15 @@
 
 let callAndApply = {
   caller(object, method, nameArg, ageArg, tShirtSizeArg) {
+    if (typeof object !== 'object' || Array.isArray(object) === true) {
+      return false;
+    }
     return method.call(object, nameArg, ageArg, tShirtSizeArg);
   },
   applier(object, method, argumentsArr) {
+    if (typeof object !== 'object' || Array.isArray(object) === true) {
+      return false;
+    }
     return method.apply(object, argumentsArr);
   },
 };
--- ex5/callapply.test.js
+++ ex5/callapply.test.js
@@ -15,3 +15,18 @@ describe('test cases for valid usage', () => {
     expect(callAndApply.applier(person, update, ['parth', '22', 'L'])).toEqual({ age: '22', name: 'parth', tShirtSize: 'L' });
   });
 });
+
+describe('test cases for invalid usage', () => {
+  test('testcase 1 for invalid input this as object', () => {
+    expect(callAndApply.caller('this', update, 'parth', '22', 'L')).toEqual(false);
+  });
+  test('testcase 2 for valid input 1 as object', () => {
+    expect(callAndApply.applier(1, update, ['parth', '22', 'L'])).toEqual(false);
+  });
+  test('testcase 3 for valid input array is object', () => {
+    expect(callAndApply.applier([1, 2, 3], update, ['parth', '22', 'L'])).toEqual(false);
+  });
+  test('testcase 2 for valid input undefined as object', () => {
+    expect(callAndApply.applier(undefined, update, ['parth', '22', 'L'])).toEqual(false);
+  });
+});
