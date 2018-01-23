c6c05c4f0939bdca874d3ee2646325f4a4fbd303
parth <srivastavaparth2016@gmail.com>
2018-01-23 15:28:47 +0530

    adding 2n test case for invocation

--- ex3/invocation.test.js
+++ ex3/invocation.test.js
@@ -4,4 +4,7 @@ describe('test cases', () => {
   test('passing numbers', () => {
     expect(invocation(1)(2)).toBe(3);
   });
+  test('passing strings', () => {
+    expect(invocation('hello')('hello')).toBe('hellohello');
+  });
 });
