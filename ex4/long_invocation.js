34dd97808cdac8aed4ff05b31bf71ea1714b629e
parth <srivastavaparth2016@gmail.com>
2018-01-23 22:24:41 +0530

    adding test case 1 for long_invocation.js

--- ex4/long_invocation.js
+++ ex4/long_invocation.js
@@ -1,9 +1,8 @@
 let total = 0;
 function invoc(arg1) {
-  if (arg1 === undefined) {
+  if (typeof arg1 !== 'number') {
     return total;
   }
-
   total += arg1;
   return invoc;
 }
---
+++ ex4/long_invocation.test.js
@@ -0,0 +1,31 @@
+let invoc = require('./long_invocation');
+
+describe('test cases for the program', () => {
+  test('testcase 1 by passing number', () => {
+    expect(typeof invoc(10)).toBe(typeof function random() {});
+  });
+  test('testcase 2 by passing nothing', () => {
+    expect(invoc()).toBe(10);
+  });
+  test('testcase 3 by passing number', () => {
+    expect(typeof invoc(10)).toBe(typeof function random() {});
+  });
+  test('testcase 4 by passing nothing', () => {
+    expect(invoc()).toBe(20);
+  });
+  test('testcase 5 byu passing strings', () => {
+    expect(invoc('stringg')).toBe(20);
+  });
+  test('testcase 6 by passing null', () => {
+    expect(invoc(null)).toBe(20);
+  });
+  test('testcase 7 by passing undefined', () => {
+    expect(invoc(undefined)).toBe(20);
+  });
+  test('testcase 8 by passing arrays', () => {
+    expect(invoc([1, 2])).toBe(20);
+  });
+  test('testcase 9', () => {
+    expect(invoc({ kk: 5 })).toBe(20);
+  });
+});
