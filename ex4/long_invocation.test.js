0c46bc2b6fd6b4b7ecf69ebb0fa720c6de9a4cfc
parth <srivastavaparth2016@gmail.com>
2018-01-24 09:06:00 +0530

    changes in describe block

--- ex4/long_invocation.test.js
+++ ex4/long_invocation.test.js
@@ -1,31 +1,31 @@
 let invoc = require('./long_invocation');
 
 describe('test cases for the program', () => {
-  test('testcase 1 by passing number', () => {
+  it('testcase 1 by passing number', () => {
     expect(typeof invoc(10)).toBe('function');
   });
-  test('testcase 2 by passing nothing', () => {
+  it('testcase 2 by passing nothing', () => {
     expect(invoc()).toBe(10);
   });
-  test('testcase 3 by passing number', () => {
+  it('testcase 3 by passing number', () => {
     expect(typeof invoc(10)).toBe('function');
   });
-  test('testcase 4 by passing nothing', () => {
+  it('testcase 4 by passing nothing', () => {
     expect(invoc()).toBe(20);
   });
-  test('testcase 5 byu passing strings', () => {
+  it('testcase 5 byu passing strings', () => {
     expect(invoc('stringg')).toBe(20);
   });
-  test('testcase 6 by passing null', () => {
+  it('testcase 6 by passing null', () => {
     expect(invoc(null)).toBe(20);
   });
-  test('testcase 7 by passing undefined', () => {
+  it('testcase 7 by passing undefined', () => {
     expect(invoc(undefined)).toBe(20);
   });
-  test('testcase 8 by passing arrays', () => {
+  it('testcase 8 by passing arrays', () => {
     expect(invoc([1, 2])).toBe(20);
   });
-  test('testcase 9', () => {
+  it('testcase 9', () => {
     expect(invoc({ kk: 5 })).toBe(20);
   });
 });
